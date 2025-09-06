da94b3a665d798ab40a3eccefa7d7bb9309f8e58
Neshaprasath-S <neshaprasathcs@gmail.com>
2025-09-06 14:42:25 +0530

    2 commit

--- local_service/settings.py
+++ local_service/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9fku4j$-zpatb!$rszh2yf)&s^c+!zd!_)_^sn)6$f9xf!1-xk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'NeshaprasathKing.pythonanywhere.com']
 
 
 # Application definition
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
